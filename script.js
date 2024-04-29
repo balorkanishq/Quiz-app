594ec90a8c4ed389d765f7b7030229ed7d8953e9
balorkanishq <80593079+balorkanishq@users.noreply.github.com>
2024-04-29 23:57:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,282 @@
+let timeLeft = document.querySelector(".time-left");
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let countOfQuestion = document.querySelector(".number-of-question");
+let displayContainer = document.getElementById("display-container");
+let scoreContainer = document.querySelector(".score-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector(".start-screen");
+let startButton = document.getElementById("start-button");
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+let countdown;
+//Questions and Options array
+const quizArray = [
+    {
+        id: "0",
+        question: "Which one of the following is an example of an internet search engine?",
+        options: ["Linux", "Google", "MS Word", "Windows"],
+        correct: "Google",
+        explanation: "Google is a popular internet search engine used worldwide."
+    },
+    {
+        id: "1",
+        question: "At which port number, Simple Network Management Protocol (SNMP) operates",
+        options: ["160", "164", "161", "162"],
+        correct: "161",
+        explanation: "SNMP uses both port 161 and port 162 for sending commands and messages. SNMP managers communicate with SNMP agents through designated SNMP ports. SNMP message transfers happen via the User Datagram Protocol (UDP)."
+    },
+    {
+        id: "2",
+        question: "Who invented Computer?",
+        options: ["Charles Babbage", "Henry Luce", "Henry Babbage", "Charles Luce"],
+        correct: "Charles Babbage",
+        explanation: "Charles Babbage, an English mechanical engineer and polymath, originated the concept of a programmable computer. "
+    },
+    {
+        id: "3",
+        question: "Which of the following is not an HTML tag?",
+        options: ["< select >", "< textarea >", "< input >", "< list >"],
+        correct: "< list >",
+        explanation: "< list > is not a HTML tag but a data structure."
+    },
+    {
+        id: "4",
+        question: "Hardware devices that are not part of the main computer system and are often added later to the system.",
+        options: ["Peripheral", "Clip art", "Highlight", "Execute"],
+        correct: "Peripheral",
+        explanation: "Peripheral devices are the hardware devices that are not part of the main computer system and are often added later to the system. E.g. Speakers are such peripheral devices."
+    },
+    {
+        id: "5",
+        question: "The main computer that stores the files that can be sent to computers that are networked together is:",
+        options: ["Clip art", "Mother board", "Peripheral", "File server"],
+        correct: "File server",
+        explanation: "A file server is a computer responsible for the storage and management of data files so that other computers on the same network can access the files. "
+    }, {
+        id: "6",
+        question: "Which among these is not a Web browser?",
+        options: ["NetSurf", "Opera", "www", "Chrome"],
+        correct: "www",
+        explanation: "wwww stands for World Wide Web which contains all web pages"
+    },
+    {
+        id: "7",
+        question: "Google (www.google.com) is a:",
+        options: ["Search Engine", "Number in Math", "Directory of images", "Chat service on the web"],
+        correct: "Search Engine",
+        explanation: "Google is a popular internet search engine used worldwide."
+    },
+    {
+        id: "8",
+        question: "Which is not an Internet protocol?",
+        options: ["HTTP", "FTP", "STP", "IP"],
+        correct: "STP",
+        explanation: "STP is not an Internet Protocol whereas HTTP(Hyper Text Transfer Protocol), FTP(File Transer Protocol) are protocols"
+    },
+    {
+        id: "9",
+        question: "What does HTML stand for?",
+        options: ["Hyper Text Markup Language", "Hyper Tabular Markup Language", "High Text Markup Language", "None of the above"],
+        correct: "Hyper Text Markup Language",
+        explanation: "HTML stands for Hyper Text Markup Language"
+    },
+];
+
+//Restart Quiz
+restart.addEventListener("click", () => {
+    initial();
+    displayContainer.classList.remove("hide");
+    scoreContainer.classList.add("hide");
+});
+
+//Next Button
+nextBtn.addEventListener(
+    "click",
+    (displayNext = () => {
+        //increment questionCount
+        questionCount += 1;
+        //if last question
+        if (questionCount == quizArray.length) {
+            //hide question container and display score
+            displayContainer.classList.add("hide");
+            scoreContainer.classList.remove("hide");
+            //user score
+            userScore.innerHTML =
+                "Your score is " + scoreCount + " out of " + questionCount;
+        } else {
+            //display questionCount
+            countOfQuestion.innerHTML =
+                questionCount + 1 + " of " + quizArray.length + " Question";
+            //display quiz
+            quizDisplay(questionCount);
+            count = 11;
+            clearInterval(countdown);
+            timerDisplay();
+        }
+    })
+);
+
+//Timer
+const timerDisplay = () => {
+    countdown = setInterval(() => {
+        count--;
+        timeLeft.innerHTML = `${count}s`;
+        if (count == 0) {
+            clearInterval(countdown);
+            displayNext();
+        }
+    }, 1000);
+};
+
+//Display quiz
+const quizDisplay = (questionCount) => {
+    let quizCards = document.querySelectorAll(".container-mid");
+    //Hide other cards
+    quizCards.forEach((card) => {
+        card.classList.add("hide");
+    });
+    //display current question card
+    quizCards[questionCount].classList.remove("hide");
+};
+
+// Quiz Creation
+function quizCreator() {
+    //randomly sort questions
+    quizArray.sort(() => Math.random() - 0.5);
+    //generate quiz
+    for (let i of quizArray) {
+        //randomly sort options
+        i.options.sort(() => Math.random() - 0.5);
+        //quiz card creation
+        let div = document.createElement("div");
+        div.classList.add("container-mid", "hide");
+        //question number
+        countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
+        //question
+        let question_DIV = document.createElement("p");
+        question_DIV.classList.add("question");
+        question_DIV.innerHTML = i.question;
+        div.appendChild(question_DIV);
+        //options
+        div.innerHTML += `
+    <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
+     <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
+      <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
+       <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
+    `;
+        quizContainer.appendChild(div);
+    }
+}
+
+//Checker Function to check if option is correct or not
+function checker(userOption) {
+    let userSolution = userOption.innerText;
+    let question =
+        document.getElementsByClassName("container-mid")[questionCount];
+    let options = question.querySelectorAll(".option-div");
+    //if user clicked answer == correct option stored in object
+    if (userSolution === quizArray[questionCount].correct) {
+        userOption.classList.add("correct");
+        scoreCount++;
+    } else {
+        userOption.classList.add("incorrect");
+        //For marking the correct option
+        options.forEach((element) => {
+            if (element.innerText == quizArray[questionCount].correct) {
+                element.classList.add("correct");
+            }
+        });
+    }
+    //clear interval(stop timer)
+    clearInterval(countdown);
+    //disable all options
+    options.forEach((element) => {
+        element.disabled = true;
+    });
+}
+
+
+// Function to hide explanation box
+function hideExplanation() {
+    let explanationBox = document.getElementById("explanation");
+    explanationBox.classList.add("hide");
+}
+
+// Next Button
+nextBtn.addEventListener(
+    "click",
+    (displayNext = () => {
+        // Hide explanation box
+        hideExplanation();
+    })
+);
+
+// Checker Function to check if option is correct or not
+function checker(userOption) {
+    let userSolution = userOption.innerText;
+    let question =
+        document.getElementsByClassName("container-mid")[questionCount];
+    let options = question.querySelectorAll(".option-div");
+    let explanationBox = document.getElementById("explanation");
+
+    if (userSolution === quizArray[questionCount].correct) {
+        userOption.classList.add("correct");
+        scoreCount++;
+    } else {
+        userOption.classList.add("incorrect");
+        options.forEach((element) => {
+            if (element.innerText == quizArray[questionCount].correct) {
+                element.classList.add("correct");
+            }
+        });
+    }
+
+    // Display explanation
+    explanationBox.innerHTML = quizArray[questionCount].explanation;
+    explanationBox.classList.remove("hide"); // Show explanation box
+
+    clearInterval(countdown);
+    options.forEach((element) => {
+        element.disabled = true;
+    });
+}
+
+// Initial setup
+function initial() {
+    quizContainer.innerHTML = "";
+    questionCount = 0;
+    scoreCount = 0;
+    count = 11;
+    clearInterval(countdown);
+    timerDisplay();
+    quizCreator();
+    quizDisplay(questionCount);
+    // Hide explanation box initially
+    hideExplanation();
+}
+
+//initial setup
+function initial() {
+    quizContainer.innerHTML = "";
+    questionCount = 0;
+    scoreCount = 0;
+    count = 11;
+    clearInterval(countdown);
+    timerDisplay();
+    quizCreator();
+    quizDisplay(questionCount);
+}
+//when user click on start button
+startButton.addEventListener("click", () => {
+    startScreen.classList.add("hide");
+    displayContainer.classList.remove("hide");
+    initial();
+});
+//hide quiz and display start screen
+window.onload = () => {
+    startScreen.classList.remove("hide");
+    displayContainer.classList.add("hide");
+};
